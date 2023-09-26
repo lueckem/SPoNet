3525ffbdb37b2b651ec814c94bf795dd21eb5b18
Marvin <marvin-luecke@web.de>
2023-09-26 11:55:50 +0200

    add Parameters to init

--- sponet/__init__.py
+++ sponet/__init__.py
@@ -1,4 +1,4 @@
-from .parameters import load_params, save_params
+from .parameters import Parameters, load_params, save_params
 from .utils import sample_many_runs, calc_rre_traj
 
 from .cnvm import CNVMParameters, CNVM
