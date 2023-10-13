42841c5423b18ec676358d94a42bbb869be1718a
Marvin <marvin-luecke@web.de>
2023-10-13 09:27:46 +0200

    plot trajectories function

--- sponet/__init__.py
+++ sponet/__init__.py
@@ -1,5 +1,5 @@
 from .parameters import Parameters, load_params, save_params
-from .utils import sample_many_runs, calc_rre_traj
+from .utils import sample_many_runs, calc_rre_traj, plot_trajectories
 
 from .cnvm import CNVMParameters, CNVM
 from .cntm import CNTMParameters, CNTM
--- sponet/utils.py
+++ sponet/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 from numba import njit
 from scipy.integrate import solve_ivp
 import multiprocessing as mp
+from matplotlib.axes import Axes
 
 from .collective_variables import CollectiveVariable
 from .parameters import Parameters
@@ -197,6 +198,65 @@ def calc_rre_traj(
 
     sol = solve_ivp(rhs, (0, t_max), c_0, rtol=1e-8, atol=1e-8, t_eval=t_eval)
     return sol.t, sol.y.T
+
+
+def plot_trajectories(
+        params: Parameters,
+        t: float,
+        cv: CollectiveVariable,
+        ax: Axes,
+        num_initial_states: int = 3,
+        samples_per_state: int = 5,
+) -> Axes:
+    """
+    Plot some trajectories with uniformly random initial states
+    in the provided Axes object.
+
+    The purpose of this function is to quickly inspect how trajectories
+    look like for the given set of Parameters.
+
+    Parameters
+    ----------
+    params : Parameters
+    t : float
+    cv : CollectiveVariable
+    ax : Axes
+    num_initial_states : int, optional
+    samples_per_state : int, optional
+
+    Returns
+    -------
+    Axes
+    """
+    initial_states = np.random.randint(
+        0,
+        params.num_opinions,
+        size=(num_initial_states, params.num_agents)
+    )
+
+    t, c = sample_many_runs(
+        params,
+        initial_states,
+        t,
+        1000,
+        samples_per_state,
+        collective_variable=cv,
+    )
+
+    colors = ["k", "b", "g", "c", "r", "y"]
+    linestyles = ["-", "--", "-."]
+
+    for i in range(num_initial_states):
+        this_linestyle = linestyles[i % len(linestyles)]
+        for j in range(samples_per_state):
+            this_color = colors[j % len(colors)]
+            ax.plot(t, c[i, j, :, 0], linestyle=this_linestyle, color=this_color)
+
+    ax.set_xlabel("$t$")
+    ax.set_ylabel("$c_0$")
+    ax.grid()
+
+    return ax
 
 
 @njit
