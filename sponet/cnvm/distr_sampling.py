6068d8380141f5ac86adb4c73fda4a87eaa637d0
Marvin <marvin-luecke@web.de>
2023-10-27 10:42:02 +0200

    table sampling is not really faster than cdf

--- sponet/cnvm/distr_sampling.py
+++ sponet/cnvm/distr_sampling.py
@@ -1,3 +1,6 @@
+import time
+
+import matplotlib.pyplot as plt
 import numpy as np
 from numba import njit
 
@@ -24,11 +27,11 @@ def rand_index_numba(prob_cum_sum) -> int:
 
 @njit()
 def rand_index_table(table, spillover_cumsum) -> int:
-    idx = np.random.choice(table)
+    idx = table[np.random.randint(0, len(table))]
     if idx > -1:
         return idx
 
-    return rand_index_numba(spillover_cumsum)
+    return np.searchsorted(spillover_cumsum, np.random.random(), side="right")
 
 
 @njit()
@@ -45,6 +48,7 @@ def construct_table(probabilities: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     table = []
     for i in range(probabilities.shape[0]):
         table.extend([i] * numbers_of_cells[i])
+    print(len(table))
     table.extend([-1] * (factor - len(table)))
 
     spill_over = probabilities * factor - np.floor(probabilities * factor)
@@ -54,23 +58,57 @@ def construct_table(probabilities: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
 
 
 def benchmark_sampling():
-    n_list = [10, 100, 1000, 10000, 100000]
+    n_list = [100, 1000, 10000, 100000]
+    timeout = 5
+
+    performance_cdf = []
+    performance_table = []
 
     for n in n_list:
-        p = np.random.random(n)
-        p /= np.sum(p)
+        print(n)
+        expected_value = n / 10
+        param_geom = 1 / expected_value
+        probability_vector = [(1 - param_geom) ** k * param_geom for k in range(n)]
+        probability_vector = np.array(probability_vector) / np.sum(probability_vector)
+
+        # cdf sampling
+        cum_sum = np.cumsum(probability_vector)
+        rand_index_numba(cum_sum)  # compile
+        num_iter = 0
+        start = time.time()
+        while time.time() < start + timeout:
+            rand_index_numba(cum_sum)
+            num_iter += 1
+        end = time.time()
+        performance_cdf.append((end - start) / num_iter)
+
+        # table sampling
+        table, spillover_cumsum = construct_table(probability_vector)
+        rand_index_table(table, spillover_cumsum)  # compile
+        num_iter = 0
+        start = time.time()
+        while time.time() < start + timeout:
+            rand_index_table(table, spillover_cumsum)
+            num_iter += 1
+        end = time.time()
+        performance_table.append((end - start) / num_iter)
+
+    plt.loglog(n_list, performance_cdf, label="cdf")
+    plt.loglog(n_list, performance_table, label="table")
+    plt.legend()
+    plt.grid()
+    plt.show()
 
 
 if __name__ == '__main__':
-    p = np.array([0.4581, 0.0032, 0.1985, 0.3298, 0.0022, 0.0080, 0.0002])
-    p = np.random.random(10000)
-    p /= np.sum(p)
-
-    table_p, spillover_cum_sum = construct_table(p)
-    results = []
-    for i in range(100):
-        results.append(rand_index_table(table_p, spillover_cum_sum))
-
-    unique, counts = np.unique(results, return_counts=True)
-    p_sample = counts / len(results)
-    print(p_sample)
+    benchmark_sampling()
+    # p = np.array([0.4581, 0.0032, 0.1985, 0.3298, 0.0022, 0.0080, 0.0002])
+    #
+    # table_p, spillover_cum_sum = construct_table(p)
+    # results = []
+    # for i in range(100):
+    #     results.append(rand_index_table(table_p, spillover_cum_sum))
+    #
+    # unique, counts = np.unique(results, return_counts=True)
+    # p_sample = counts / len(results)
+    # print(p_sample)
