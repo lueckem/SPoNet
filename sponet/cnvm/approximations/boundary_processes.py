99129c51014819de6d49f5b5b27a18d901be888d
MUrban <manuel.urban@uni-bayreuth.de>
2025-09-19 09:36:37 +0200

    bump

--- sponet/cnvm/approximations/boundary_processes.py
+++ sponet/cnvm/approximations/boundary_processes.py
@@ -70,21 +70,6 @@ def clip_to_boundary(
     return x_store, t_after_breach, clipped_state, next_store_index, False
 
 
-def compute_boundary_reflection(
-    _t_eval: NDArray,
-    x_store: NDArray,
-    _t_before_breach: float,
-    t_after_breach: float,
-    _state_before_breach: NDArray,
-    state_after_breach: NDArray,
-    next_store_index: int,
-    n_nodes: int,
-    r: NDArray,
-    r_tilde: NDArray,
-) -> tuple[NDArray, float, NDArray, int, bool]:
-    pass
-
-
 @njit(cache=True)
 def compute_normal_boundary_reflection(
     _t_eval: NDArray,
