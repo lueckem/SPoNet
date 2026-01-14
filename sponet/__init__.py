c7e2688e2a0b67c6acbb71dff795c0e1b8989070
marvin <marvin-luecke@web.de>
2026-01-14 16:38:00 +0100

    __init__

--- sponet/__init__.py
+++ sponet/__init__.py
@@ -8,7 +8,32 @@ from .cnvm import (
     sample_cle,
     sample_stochastic_approximation,
 )
+from .collective_variables import (
+    CompositeCollectiveVariable,
+    DegreeWeightedOpinionShares,
+    Interfaces,
+    OpinionShares,
+    OpinionSharesByDegree,
+)
 from .multiprocessing import sample_many_runs
+from .network_generator import (
+    BarabasiAlbertGenerator,
+    BianconiBarabasiGenerator,
+    BinomialWattsStrogatzGenerator,
+    ErdosRenyiGenerator,
+    GridGenerator,
+    RandomRegularGenerator,
+    StochasticBlockGenerator,
+    WattsStrogatzGenerator,
+)
 from .parameters import Parameters, load_params, save_params
 from .sample_moments import sample_moments
+from .states import (
+    build_state_by_degree,
+    sample_states_local_clusters,
+    sample_states_target_cvs,
+    sample_states_target_shares,
+    sample_states_uniform,
+    sample_states_uniform_shares,
+)
 from .steady_state import estimate_steady_state
